21ffb018f999097c89c07d37c9dd49336f46e204
danciueduard <danciu.euu@gmail.com>
2024-01-12 22:01:35 +0200

    Created the Services menu

--- src/pages/servicesPage/ServicesMenu.jsx
+++ src/pages/servicesPage/ServicesMenu.jsx
@@ -1,51 +1,87 @@
 import styled from "styled-components";
+import { IoRestaurantOutline } from "react-icons/io5";
+import { CgPushDown } from "react-icons/cg";
+import { PiGasCan } from "react-icons/pi";
+import { GiAutoRepair } from "react-icons/gi";
+import { useState } from "react";
 
-const StyledMenu = styled.div`
+const StyledMenu = styled.li`
   display: flex;
+  gap: 3rem;
   justify-content: center;
   align-items: center;
   padding: 0 2rem;
-  /* gap: 0.9rem; */
   height: 2rem;
   text-transform: uppercase;
   font-size: 1.2rem;
-  /* background-color: var(--secondary-color); */
   margin: 2rem;
   width: 400px;
   border-radius: 1rem;
-  position: relative;
-  overflow: hidden;
 
-  p {
-    /* flex: 1; */
-    /* background-color: red; */
-    justify-content: center;
-    align-items: center;
-    padding: 0.5rem 0.5rem;
-    margin: 0;
-  }
-  .selected {
-    /* background-color: blue; */
-    text-decoration: underline;
-    color: blue;
+  .active {
+    color: var(--secondary-color);
+    transform: scale(120%);
   }
 `;
+const MenuItem = styled.div`
+  color: var(--primary-color);
+  font-size: 3rem;
+  filter: drop-shadow(3px 5px 2px rgb(0 0 0 / 0.06));
+`;
 
-const DivContainer = styled.div`
+const Container = styled.div`
   display: flex;
   justify-content: center;
 `;
+const HorizontalLine = styled.div`
+  position: relative;
+  bottom: 0;
+  left: 0;
+  width: 27rem;
+  height: 1px;
+  background-color: #ccc;
+`;
 
 function ServicesMenu() {
+  const [activeItem, setActiveItem] = useState(null);
+  function handleClick(item) {
+    setActiveItem(item);
+  }
+
   return (
-    <DivContainer>
-      <StyledMenu>
-        <p>Catering</p>
-        <p className="selected">Pushback</p>
-        <p>Fuel</p>
-        <p>Meintenance</p>
-      </StyledMenu>
-    </DivContainer>
+    <>
+      <Container>
+        <StyledMenu>
+          <MenuItem
+            className={activeItem === "catering" ? "active" : ""}
+            onClick={() => handleClick("catering")}
+          >
+            <IoRestaurantOutline />
+          </MenuItem>
+          <MenuItem
+            className={activeItem === "pushback" ? "active" : ""}
+            onClick={() => handleClick("pushback")}
+          >
+            <CgPushDown />
+          </MenuItem>
+          <MenuItem
+            className={activeItem === "fuel" ? "active" : ""}
+            onClick={() => handleClick("fuel")}
+          >
+            <PiGasCan />
+          </MenuItem>
+          <MenuItem
+            className={activeItem === "meintenance" ? "active" : ""}
+            onClick={() => handleClick("meintenance")}
+          >
+            <GiAutoRepair />
+          </MenuItem>
+        </StyledMenu>
+      </Container>
+      <Container>
+        <HorizontalLine />
+      </Container>
+    </>
   );
 }
 
